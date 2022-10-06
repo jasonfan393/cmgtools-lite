07bd374626b01eb49ce9225d0f5709df902b55ec
Divya <dg669@cornell.edu>
2022-10-06 22:54:14 +0200

    triggerSF for UL

--- TTHAnalysis/python/plotter/ttH-multilepton/functionsTTH.cc
+++ TTHAnalysis/python/plotter/ttH-multilepton/functionsTTH.cc
@@ -933,4 +933,121 @@ float ttH_3l_clasifier(float nJet25,float nBJetMedium25){
   else return -1;
 }
 
+float triggerSF_ttH(int pdgid1, float pt1, int pdgid2, float pt2, int nlep, int year, int suberaid, int var=0){
+
+  TString yearString= TString::Format("%d",year) + (( year == 2016 && suberaid == 0) ? "APV" : "");
+
+  if (nlep == 2){
+    if (abs(pdgid1*pdgid2) == 121){
+
+      if (yearString == "2016APV"){
+        if (pt2 < 20){
+          return 0.96*(1 + var*0.02);
+        }
+        else if (pt2 > 20 && pt2 < 55){
+          return 0.99*(1 + var*0.01);
+        }
+        else return 1.*(1 + var*0.01);
+      }
+
+      if (yearString == "2016"){
+        if (pt2 < 40){
+          return 0.98*(1 + var*0.02);
+        }
+        else return 0.99*(1 + var*0.01);
+      }
+
+      if (yearString == "2017"){
+        if (pt2<25) return 0.96*(1 + var*0.02);
+	else return 0.985*(1 + var*0.01);
+      }
+
+      if (yearString == "2018"){
+        if (pt2<20){
+	  return 0.98*(1 + var*0.01);
+        }
+        else if (pt2 > 20 && pt2 < 70){
+          return 1.*(1 + var*0.01);
+        }
+        else return 1.01*(1 + var*0.005);
+      }
+    }
+
+    else if ( abs(pdgid1*pdgid2) == 143){
+
+      if (yearString == "2016APV"){
+        if (pt2 < 25){
+          return 0.98*(1 + var*0.01);
+        }
+        else if (pt2 > 25 && pt2 < 70){
+          return 0.99*(1 + var*0.005);
+        }
+        else return 1.*(1 + var*0.005);
+      }
+
+      if (yearString == "2016"){
+        if (pt2 < 20){
+          return 0.98*(1 + var*0.02);
+        }
+        else return 0.99*(1 + var*0.01);
+      }
+
+      if (yearString == "2017"){
+	if (pt2<20) return 0.99*(1 + var*0.01);
+        else if (pt2 > 20 && pt2 < 40){
+          return 0.98*(1 + var*0.01);
+        }
+        else return 0.995*(1 + var*0.005);
+      }
+
+      if (yearString == "2018"){
+        if (pt2<20) return 0.98*(1 + var*0.01);
+        else if (pt2 > 20 && pt2 < 55){
+          return 0.99*(1 + var*0.005);
+        }
+        else  return 1.*(1 + var*0.005);
+      }
+    }
+
+    else{
+      if (yearString == "2016APV"){
+        if (pt2 < 25){
+          return 0.98*(1 + var*0.01);
+        }
+        else return 0.99*(1 + var*0.01);
+      }
+
+      if (yearString == "2016"){
+        if (pt2 < 20){
+          return 0.97*(1 + var*0.01);
+        }
+        else return 0.99*(1 + var*0.01);
+      }
+
+      if (yearString == "2017"){
+        if (pt2 < 25){
+          return 0.97*(1 + var*0.01);
+        }
+        else return 0.99*(1 + var*0.01);
+      }
+
+      if (yearString == "2018"){
+        return 0.99*(1 + var*0.01);
+      }
+    }
+
+  }
+  else {
+    
+    if (yearString == "2016APV" || yearString == "2016"){
+      return 1.*(1 + var*0.02);
+    }
+    if (yearString == "2017" || yearString == "2018"){
+      return 1.*(1 + var*0.01);
+    }
+
+  }
+
+}
+
 

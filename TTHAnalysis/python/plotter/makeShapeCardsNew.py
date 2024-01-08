8ec741deaaff8ceaab48091584f9bbd1702fcfa7
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2024-01-08 14:29:47 +0100

    Upload bias test code

--- TTHAnalysis/python/plotter/makeShapeCardsNew.py
+++ TTHAnalysis/python/plotter/makeShapeCardsNew.py
@@ -9,7 +9,6 @@ parser = OptionParser(usage="%prog [options] mc.txt cuts.txt var bins")
 addMCAnalysisOptions(parser)
 parser.add_option("--od", "--outdir", dest="outdir", type="string", default=None, help="output directory name") 
 parser.add_option("--asimov", dest="asimov", type="string", default=None, help="Use an Asimov dataset of the specified kind: including signal ('signal','s','sig','s+b') or background-only ('background','bkg','b','b-only')")
-parser.add_option("--use-alternative-signal", dest="alternative_signal", type="string", default=None, help="Select which signal to use for computing the asimov.")
 parser.add_option("--use-alternative-mca", dest="alternative_mca", type="string", default=None, help="MCA with other signal models.")
 parser.add_option("--bbb", dest="bbb", type="string", default=None, help="Options for bin-by-bin statistical uncertainties with the specified nuisance name")
 parser.add_option("--amc", "--autoMCStats", dest="autoMCStats", action="store_true", default=False, help="use autoMCStats")
@@ -63,22 +62,30 @@ if options.asimov:
     
     # Case 1: signal + background
     if options.asimov in ("s","sig","signal","s+b"):
+
         asimovprocesses = mca.listSignals() + mca.listBackgrounds()
+        print(" Started asimov: ", asimovprocesses)
     
-        if options.alternative_signal and options.alternative_mca:
-            print("     - Will use signal %s for the asimov only (fit will use nominal sample)"%options.alternative_signal)
-            print("     - Will use mca %s"%options.alternative_mca)
+        if options.alternative_mca:
+            print("     - Will use signals defined in %s for the asimov only (fit will use nominal sample)"%options.alternative_mca)
             otherMca = MCAnalysis(options.alternative_mca, options)
-            match = re.compile(options.alternative_signal)
-            new_signals = list(filter(match.match, otherMca.listSignals()))
             
             # Change the list of signals
-            asimovprocesses = new_signals + ["TTW_ooa"] + mca.listBackgrounds()
-            other_report = otherMca.getPlotsRaw("x", args[2], args[3], cuts.allCuts(), nodata=options.asimov) 
+            asimovprocesses = otherMca.listSignals() + mca.listBackgrounds()
+
+            other_report = None
+            if options.categ:
+                cexpr, cbins, _ = options.categ
+                other_report= otherMca.getPlotsRaw("x", cexpr+":"+args[2], makeBinningProductString(args[3],cbins), cuts.allCuts(), nodata=options.asimov) 
+            else:
+                other_report = otherMca.getPlotsRaw("x", args[2], args[3], cuts.allCuts(), nodata=options.asimov) 
+                        
             for p,h in other_report.iteritems(): h.cropNegativeBins(threshold=1e-5)
-            report[options.alternative_signal] = other_report[options.alternative_signal]
             
-        print(asimovprocesses) 
+            report[otherMca.listSignals()[0]] = other_report[otherMca.listSignals()[0]]
+            
+        print("With alt signal:", asimovprocesses) 
+    
     # Case 2: only background asimov
     elif options.asimov in ("b","bkg","background", "b-only"):
         asimovprocesses = mca.listBackgrounds()
@@ -92,14 +99,17 @@ if options.asimov:
         if p in report: 
             if tomerge is None: 
                 tomerge = report[p].raw().Clone("x_data_obs"); tomerge.SetDirectory(None)
-            else: 
+            else:
+                print(" -- process: ", p) 
+                print("   +: ", report[p].raw()) 
                 tomerge.Add(report[p].raw())
                 
     report['data_obs'] = HistoWithNuisances(tomerge)
-    if options.alternative_signal and options.alternative_mca:
-        # Turn back to common usage now the observation asimov has been submitted
+    if options.alternative_mca:
+        # Turn back to common usage now the observation asimov has been computed
         asimovprocesses = mca.listSignals() + mca.listBackgrounds()
-        report.pop(options.alternative_signal)
+        report.pop(otherMca.listSignals()[0])
+        print(" Finished asimov: ", asimovprocesses)
 else:
     report['data_obs'] = report['data'].Clone("x_data_obs") 
 

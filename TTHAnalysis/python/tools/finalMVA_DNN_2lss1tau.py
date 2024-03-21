0d5c3d90bd443d59944ef42af550a7f0f2af3b5c
Jason Fan <jasonfan393@gmail.com>
2024-03-21 20:06:45 +0100

    fix to work on all years

--- TTHAnalysis/python/tools/finalMVA_DNN_2lss1tau.py
+++ TTHAnalysis/python/tools/finalMVA_DNN_2lss1tau.py
@@ -11,51 +11,11 @@ from copy import deepcopy
 import ROOT as r 
 
 
-def mTauTauVis( ev, ind, hist): 
-    #file = r.TFile("/home/ucl/cp3/atalier/final_tth/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/data/TauIDSFs/TauES_dm_DeepTau2017v2p1VSjet_UL2018.root")
-    #hist = file.Get('tes')
-    #    tes  = hist.GetBinContent(hist.GetXaxis().FindBin(dm))
-
-    all_leps = [l for l in Collection(ev,"LepGood")]
-    nFO = getattr(ev,"nLepFO_Recl")
-    chosen = getattr(ev,"iLepFO_Recl")
-    leps = [all_leps[chosen[i]] for i in xrange(nFO)]
-    if len(leps) < 2: return -1
-    
-    if ev.Tau_tight2lss1tau_idx < 0: return -1
-    taus = [ t for t in Collection(ev, 'TauSel_Recl')]
-    dms = [ d for d in Collection(ev, 'TauSel_Recl_decayMode')]
-
-    #dm = ev.TauSel_Recl_decayMode
-    #tes  = hist.GetBinContent(hist.GetXaxis().FindBin(dm))
-    #print("ciao ", tes)
-    return (leps[ind].p4() + (taus[int(ev.Tau_tight2lss1tau_idx)].p4()*hist.GetBinContent(hist.GetXaxis().FindBin(dms[int(ev.Tau_tight2lss1tau_idx)])) )).M()  
-
-def massL3( ev, var, hist): 
-    if ev.Tau_tight2lss1tau_idx < 0: return -1
-    all_leps = [l for l in Collection(ev,"LepGood")]
-    nFO = getattr(ev,"nLepFO_Recl")
-    chosen = getattr(ev,"iLepFO_Recl")
-    leps = [all_leps[chosen[i]] for i in xrange(nFO)]
-    if len(leps) < 2: return 0
-
-    taus = [ t for t in Collection(ev, 'TauSel_Recl')]
-    dms = [ d for d in Collection(ev, 'TauSel_Recl_decayMode')]
-    l1=r.TLorentzVector();l2=r.TLorentzVector()
-    l1.SetPtEtaPhiM(leps[0].conePt, leps[0].eta, leps[0].phi, 0)
-    l2.SetPtEtaPhiM(leps[1].conePt, leps[1].eta, leps[1].phi, 0)
-    part = l1 + l2 + taus[int(ev.Tau_tight2lss1tau_idx)].p4()*hist.GetBinContent(hist.GetXaxis().FindBin(dms[int(ev.Tau_tight2lss1tau_idx)]))
-    
-    met_pt  = getattr(ev,'MET_pt%s'%var) 
-    met_phi = getattr(ev,'MET_phi%s'%var)
-
-    return sqrt(part.Pt()*met_pt*(1-cos(part.Phi()-met_phi)))
 
 
 class finalMVA_DNN_2lss1tau(Module):
     def __init__(self, tesYear = "2018", variations=[], doSystJEC=True, fillInputs=False):
-        file = r.TFile("/home/ucl/cp3/atalier/final_tth/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/data/TauIDSFs/TauES_dm_DeepTau2017v2p1VSjet_UL{}.root".format(tesYear))
-        self.tesHist = file.Get('tes')
+        rootFile = "/home/ucl/cp3/atalier/final_tth/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/data/TauIDSFs/TauES_dm_DeepTau2017v2p1VSjet_UL{}.root".format(tesYear)
         self.outVars = []
         self._MVAs   = []
         self.fillInputs = fillInputs
@@ -107,8 +67,52 @@ class finalMVA_DNN_2lss1tau(Module):
 
         self._MVAs.extend( [ worker_2lss1tau_unclUp, worker_2lss1tau_unclDown])
         self.outVars.extend( ['DNN_2lss1tau_unclUp_' + x for x in cats_2lss1tau] +  ['DNN_2lss1tau_unclDown_' + x for x in cats_2lss1tau])
-
-
+    def applyTES(self, ev):
+        file = ROOT.TFile(self.rootFile)
+        hist = file.Get('tes')
+        return hist.GetBinContent(hist.GetXaxis().FindBin(ev.TauSel_Recl_decayMode[int(ev.Tau_tight2lss1tau_idx)]))
+    def mTauTauVis( ev, ind): 
+        file = r.TFile(self.rootFile)
+        hist = file.Get('tes')
+        #    tes  = hist.GetBinContent(hist.GetXaxis().FindBin(dm))
+    
+        all_leps = [l for l in Collection(ev,"LepGood")]
+        nFO = getattr(ev,"nLepFO_Recl")
+        chosen = getattr(ev,"iLepFO_Recl")
+        leps = [all_leps[chosen[i]] for i in xrange(nFO)]
+        if len(leps) < 2: return -1
+        
+        if ev.Tau_tight2lss1tau_idx < 0: return -1
+        taus = [ t for t in Collection(ev, 'TauSel_Recl')]
+        dms = [ d for d in Collection(ev, 'TauSel_Recl_decayMode')]
+    
+        #dm = ev.TauSel_Recl_decayMode
+        #tes  = hist.GetBinContent(hist.GetXaxis().FindBin(dm))
+        #print("ciao ", tes)
+        return (leps[ind].p4() + (taus[int(ev.Tau_tight2lss1tau_idx)].p4()*hist.GetBinContent(hist.GetXaxis().FindBin(dms[int(ev.Tau_tight2lss1tau_idx)])) )).M()  
+    
+    def massL3( ev, var): 
+        file = r.TFile(self.rootFile)
+        hist = file.Get('tes')
+        if ev.Tau_tight2lss1tau_idx < 0: return -1
+        all_leps = [l for l in Collection(ev,"LepGood")]
+        nFO = getattr(ev,"nLepFO_Recl")
+        chosen = getattr(ev,"iLepFO_Recl")
+        leps = [all_leps[chosen[i]] for i in xrange(nFO)]
+        if len(leps) < 2: return 0
+    
+        taus = [ t for t in Collection(ev, 'TauSel_Recl')]
+        dms = [ d for d in Collection(ev, 'TauSel_Recl_decayMode')]
+        l1=r.TLorentzVector();l2=r.TLorentzVector()
+        l1.SetPtEtaPhiM(leps[0].conePt, leps[0].eta, leps[0].phi, 0)
+        l2.SetPtEtaPhiM(leps[1].conePt, leps[1].eta, leps[1].phi, 0)
+        part = l1 + l2 + taus[int(ev.Tau_tight2lss1tau_idx)].p4()*hist.GetBinContent(hist.GetXaxis().FindBin(dms[int(ev.Tau_tight2lss1tau_idx)]))
+        
+        met_pt  = getattr(ev,'MET_pt%s'%var) 
+        met_phi = getattr(ev,'MET_phi%s'%var)
+    
+        return sqrt(part.Pt()*met_pt*(1-cos(part.Phi()-met_phi)))
+    
     def getVarsForVariation(self, var):
         return {'avg_dr_jet'             : lambda ev : getattr(ev,'avg_dr_jet%s'%var),
                 'min_dr_Lep'             : lambda ev : min([ev.drlep12, ev.drlep13, ev.drlep23]),
@@ -144,13 +148,13 @@ class finalMVA_DNN_2lss1tau(Module):
                 'nJetForward'            : lambda ev : getattr(ev,'nFwdJet%s_Recl'%var),
                 "mT_lep1"                : lambda ev : getattr(ev,'MT_met_lep1%s'%var),
                 "mT_lep2"                : lambda ev : getattr(ev,'MT_met_lep2%s'%var),
-                'tau1_pt'                : lambda ev : ev.TauSel_Recl_pt [int(ev.Tau_tight2lss1tau_idx)]*self.tesHist.GetBinContent(self.tesHist.GetXaxis().FindBin(ev.TauSel_Recl_decayMode[int(ev.Tau_tight2lss1tau_idx)])) if ev.Tau_tight2lss1tau_idx > -1 else 0, 
+                'tau1_pt'                : lambda ev : ev.TauSel_Recl_pt [int(ev.Tau_tight2lss1tau_idx)] * self.applyTES(ev) if ev.Tau_tight2lss1tau_idx > -1 else 0, 
                 'tau1_eta'               : lambda ev : ev.TauSel_Recl_eta[int(ev.Tau_tight2lss1tau_idx)] if ev.Tau_tight2lss1tau_idx > -1 else 0, 
                 'tau1_phi'               : lambda ev : ev.TauSel_Recl_phi[int(ev.Tau_tight2lss1tau_idx)] if ev.Tau_tight2lss1tau_idx > -1 else 0, 
                 'mindr_tau_jet'          : lambda ev : getattr(ev,'mindr_tau_jet%s'%var),
-                'mTauTauVis1'            : lambda ev : mTauTauVis(ev, 0, self.tesHist),
-                'mTauTauVis2'            : lambda ev : mTauTauVis(ev, 1, self.tesHist),
-                'massL3'                 : lambda ev : massL3(ev, var, self.tesHist),
+                'mTauTauVis1'            : lambda ev : self.mTauTauVis(ev, 0),
+                'mTauTauVis2'            : lambda ev : self.mTauTauVis(ev, 1),
+                'massL3'                 : lambda ev : self.massL3(ev, var),
 
             }
 
